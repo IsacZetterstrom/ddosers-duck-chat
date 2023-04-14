aca26cb81c421bf6bbc71bead9eb4af28a7b4f38
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2023-04-14 09:42:07 +0200

    Added more error handling to jwt auth

--- backend/Express-server/src/filter/authenticated.js
+++ backend/Express-server/src/filter/authenticated.js
@@ -3,15 +3,20 @@ import jwtUtil from "../utils/jwtUtil.js";
 function authenticate(req, res, next) {
   const token = req.headers.authorization;
   if (!token) {
-    res.send("You need to supply a token!");
+    res.status(400).send("You need to supply a token!");
     return;
   }
-  const payload = jwtUtil.verify(token);
-  if (payload.username) {
-    req.jwtPayload = payload;
-    next();
-  } else {
-    res.status(401).send("Your token is invalid!");
+  try {
+    const payload = jwtUtil.verify(token);
+    if (payload.username) {
+      req.jwtPayload = payload;
+      next();
+    } else {
+      res.status(400).send("Your token is invalid!");
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(400).send("Bad token");
   }
 }
 
--- backend/Express-server/src/utils/jwtUtil.js
+++ backend/Express-server/src/utils/jwtUtil.js
@@ -5,7 +5,10 @@ const privateKey = fs.readFileSync("./config/private.pem");
 const publicKey = fs.readFileSync("./config/public.pem");
 
 function createToken(payload) {
-  return Jwt.sign(payload, privateKey, { algorithm: "RS256" });
+  return Jwt.sign(payload, privateKey, {
+    algorithm: "RS256",
+    expiresIn: "900s",
+  });
 }
 
 function verify(token) {
